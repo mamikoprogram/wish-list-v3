5211978d53b238a756a3ae0d0416fc38c3b3d9d1
mamiko <tokunaga8950@gmail.com>
2022-06-24 00:19:05 +0900

    #36 wish追加(テーブル作成完了)

---
+++ src/learning/sql/wishes.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: db
+-- 生成日時: 2022 年 6 月 23 日 0:13
+-- サーバのバージョン： 5.7.36
+-- PHP のバージョン: 8.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `learning_php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `wishes`
+--
+
+CREATE TABLE `wishes` (
+                          `id` int(10) NOT NULL,
+                          `my_wish` varchar(255) DEFAULT NULL,
+                          `memo` varchar(255) DEFAULT NULL,
+                          `completion` varchar(12) DEFAULT NULL,
+                          `user_id` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `wishes`
+--
+ALTER TABLE `wishes`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `wishes`
+--
+ALTER TABLE `wishes`
+    MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
