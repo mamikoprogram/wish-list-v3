0f7b7f1e9d32b85633b89c058c9aad603c414ff9
mamiko <tokunaga8950@gmail.com>
2022-07-31 22:29:26 +0900

    #40 Wish一覧（作業完了）

--- src/include/view.php
+++ src/include/view.php
@@ -6,6 +6,13 @@ function render(string $template, array $vars = []): void
     require dirname(__FILE__) . "/../templates/{$template}.php";
 }
 
+/**
+ * 部品化されたHTMLを出力する
+ *
+ * @param string $element 要素名
+ * @param array $vars 利用される変数を連想配列で指定
+ * @return void
+ */
 function element(string $element, array $vars = []): void
 {
     extract($vars, EXTR_OVERWRITE);
