4bb47b49b1c1981154f4dfaae2b21b3d2f99102e
mamiko <tokunaga8950@gmail.com>
2022-08-07 23:19:31 +0900

    #41 Wish詳細(作業中)

--- src/learning/wish/detail.php
+++ src/learning/wish/detail.php
@@ -1,10 +1,22 @@
 <?php
+
 require_once "../../include/initialize.php";
 
 render('wish/detail', [
     'id' => $_GET['id'],
 ]);
-var_dump($_GET['id']);
+
+//Wish詳細を取得
+    $sql = "SELECT * FROM wishes WHERE  id = :ID";
+try {
+    $stmt = select($db, $sql, [':ID' => 'id']);
+} catch (Exception $e) {
+}
+$detail = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
-<!--todo select修正して使う？-->
+
+
+
+
+
--- src/templates/wish/detail.php
+++ src/templates/wish/detail.php
@@ -4,12 +4,13 @@ element('header', ['title' => 'Detail Wish']);
 ?>
 <?php
 //todo if文の中身修正　対象のWishを取得する
-if ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+if ($detail = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
     <p class="detail"><?php
-        echo $row['subject']; ?></p><br>
+        echo $detail['subject']; ?></p><br>
     <h2>Memo</h2>
     <p class="detail"><?php
-        echo $row['memo']; ?></p><br>
-    <?php endif; ?>
+        echo $detail['memo']; ?></p><br>
+<?php
+endif; ?>
 <a href="http://localhost:8080/wish/index.php" class="btn-style">もどる</a>
 
