21d96741e48c14711f5897c609f991ed0185e1d5
mamiko <tokunaga8950@gmail.com>
2022-08-13 00:23:18 +0900

    #41 Wish詳細(作業中)

--- src/learning/wish/detail.php
+++ src/learning/wish/detail.php
@@ -2,21 +2,14 @@
 
 require_once "../../include/initialize.php";
 
+//Wish詳細を取得
+$id = $_GET['id'];
+$db = db();
+$sql = 'SELECT * FROM wishes WHERE id = :ID';
+$stmt = select($db, $sql, [':ID' => $id,]);
+$detail = $stmt->fetch(PDO::FETCH_ASSOC);
+
 render('wish/detail', [
     'id' => $_GET['id'],
+    'detail' => $detail,
 ]);
-
-//Wish詳細を取得
-    $sql = "SELECT * FROM wishes WHERE  id = :ID";
-try {
-    $stmt = select($db, $sql, [':ID' => 'id']);
-} catch (Exception $e) {
-}
-$detail = $stmt->fetch(PDO::FETCH_ASSOC);
-?>
-
-
-
-
-
-
--- src/templates/wish/detail.php
+++ src/templates/wish/detail.php
@@ -6,11 +6,10 @@ element('header', ['title' => 'Detail Wish']);
 //todo if文の中身修正　対象のWishを取得する
 if (!empty($detail)): ?>
     <p class="detail"><?php
-        echo $detail['subject']; ?></p><br>
+        echo h($detail['subject']); ?></p><br>
     <h2>Memo</h2>
     <p class="detail"><?php
-        echo $detail['memo']; ?></p><br>
+        echo h($detail['memo']); ?></p><br>
 <?php
 endif; ?>
 <a href="http://localhost:8080/index.php" class="btn-style">もどる</a>
-
