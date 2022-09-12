dbc58eea508b37c610dde97f06f6abc6bebe4d25
mamiko <tokunaga8950@gmail.com>
2022-09-12 12:45:44 +0900

    #44 Wish編集（作業中）

--- src/learning/wish/edit.php
+++ src/learning/wish/edit.php
@@ -2,18 +2,27 @@
 
 require_once "../../include/initialize.php";
 
-if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+$db = db();
+//編集画面の内容を取得
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     return null;
 }
-
-$db = db();
 $row = getWishById($db, $_GET['id'], $_SESSION['id']);
 
-$row = updateWish($db, $_GET['id'], $_SESSION['id']);
-//var_dump($update);
-if ($row === 0) {
+$numRow = updateWish($db, $_GET['id'], $_SESSION['id']);
+
+render('wish/edit', [
+    'row' => $row,
+    'id' => $_GET['id'],
+    'wishes' => $wishes
+]);
+
+if ($numRow === 0) {
     echo '更新できませんでした。';
 }
-render('wish/edit', ['row' => $row]);
-header('location:http://localhost:8080/wish/detail.php');
-exit;
+
+if ($numRow >= 1) {
+    header('location:http://localhost:8080/wish/detail.php');
+    exit;
+}
